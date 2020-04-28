d333a4701eb293b7990ceab9ee2acf76c19c3c17
freddycastro11 <Freddyenrique2000@gmail.com>
2020-04-28 11:58:39 -0600

    odvan

---
+++ public/vistas/clientes/cliente.js
@@ -0,0 +1,13 @@
+var appcliente = new Vue({
+            el: '#frm-clientes',
+            data: {
+                usuario: {
+                    idCliente: 0,
+                    accion: 'nuevo',
+                    nombre: '',
+                    direccion: '',
+                    telefono: '',
+                    dui: ''
+                }
+            },
+});
